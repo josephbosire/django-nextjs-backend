770b827f36158043a63fec90fd4f2ce29b1571cd
Joseph Bosire <josephbosire@josephs-mbp.home>
2024-08-19 15:53:52 -0400

    Prepare for railway prod

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".railway.app"]
 
 
 # Application definition
